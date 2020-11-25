6fe772fe828ba4125925211b9b88770e926e8ba2
Jan Høydahl <jan.git@cominvent.com>
2020-11-25 10:56:12 +0100

    Undo whitespace changes

--- src/main/java/org/jlibvips/jna/glib/GLibBindingsSingleton.java
+++ src/main/java/org/jlibvips/jna/glib/GLibBindingsSingleton.java
@@ -4,25 +4,25 @@ import com.sun.jna.Native;
 
 public class GLibBindingsSingleton {
 
-    private static final String ENV_GLIBC_PATH = "JLIBVIPS_GLIBC_PATH";
-    private static String libraryPath = System.getenv(ENV_GLIBC_PATH) == null
-            ? "libglib-2.0"
-            : System.getenv(ENV_GLIBC_PATH);
+  private static final String ENV_GLIBC_PATH = "JLIBVIPS_GLIBC_PATH";
+  private static String libraryPath = System.getenv(ENV_GLIBC_PATH) == null
+          ? "libglib-2.0"
+          : System.getenv(ENV_GLIBC_PATH);
 
-    public static void configure(String lp) {
-        libraryPath = lp;
-    }
+  public static void configure(String lp) {
+      libraryPath = lp;
+  }
 
-    private static GLibBindings INSTANCE;
+  private static GLibBindings INSTANCE;
 
-    public static GLibBindings instance() {
-        if(INSTANCE == null) {
-            INSTANCE = Native.load(libraryPath, GLibBindings.class);
-        }
-        return INSTANCE;
-    }
+  public static GLibBindings instance() {
+      if(INSTANCE == null) {
+          INSTANCE = Native.load(libraryPath, GLibBindings.class);
+      }
+      return INSTANCE;
+  }
 
-    private GLibBindingsSingleton() {
-    }
+  private GLibBindingsSingleton() {
+  }
 
 }
